5afc3499d0460c2dde288d29858bbc13d75ce178
Snehith Challa <challasnehith@gmail.com>
2024-08-21 22:27:01 -0700

    Add beautiful Login page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LanguageSelectionPage from '../src/components/LanguageSelectionPage';
 import CurriculumPage from '../src/components/CurriculumPage';
+import LoginPage from '../src/components/LoginPage';
 
 export function App(props) {
   return (
       <Router>
         <div className='App'>
           <Routes>
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/" element={<LanguageSelectionPage />} />
             <Route path="/curriculum/:languageId" element={<CurriculumPage />} />
           </Routes>
